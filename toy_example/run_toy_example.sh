d71100355312b91b78129d1692026f5b35fcf187
vagrant <vagrant@vagrant.vm>
2021-04-11 03:32:18 +0000

    fix np2 and run_toy_example.sh

--- toy_example/run_toy_example.sh
+++ toy_example/run_toy_example.sh
@@ -3,6 +3,8 @@
 p_in_expr_target=NetProphet_2.0/toy_example/zev_expr_500_100_indexed
 p_in_expr_reg=NetProphet_2.0/toy_example/zev_expr_reg_50_100_indexed
 
+# de
+p_in_net_de=NetProphet_2.0/toy_example/zev_de_shrunken_50_500_indexed
 # lasso
 flag_global_shrinkage="ON"
 flag_local_shrinkage="OFF"
@@ -14,7 +16,7 @@ p_in_dbd_pids=NetProphet_2.0/toy_example/DBD_PIDS
 p_in_promoter=NetProphet_2.0/toy_example/promoter.scer.fasta
 
 # logistics
-p_src_code=/home/NetProphet_2.0/
+p_src_code=/home/vagrant/NetProphet_2.0/
 
 # singularity
 flag_singularity="ON"
@@ -24,15 +26,20 @@ p_singularity_bindpath=/home/vagrant/
 # output
 p_out_dir=/home/vagrant/res_toy_example/
 
-${p_src_code}np2 \
+cmd="${p_src_code}np2 \
     --p_in_expr_target ${p_in_expr_target} \
     --p_in_expr_reg ${p_in_expr_reg} \
+    --p_in_net_de ${p_in_net_de} \
     --flag_global_shrinkage ${flag_global_shrinkage} \
     --flag_local_shrinkage ${flag_local_shrinkage} \
+    --bart_nbr_rmpi_slave 2 \
     --p_in_dbd_pids ${p_in_dbd_pids} \
     --p_in_promoter ${p_in_promoter} \
     --p_src_code ${p_src_code} \
     --flag_singularity ${flag_singularity} \
     --p_singularity_img ${p_singularity_img} \
     --p_singularity_bindpath ${p_singularity_bindpath} \
-    --p_out_dir ${p_out_dir}
+    --p_out_dir ${p_out_dir}"
+
+echo ${cmd}
+eval ${cmd}
