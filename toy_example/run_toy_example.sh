8c31831067386d9187b6a81c17c84e595589ed96
vagrant <vagrant@vagrant.vm>
2021-04-11 03:41:13 +0000

    fix bus in run_toy_example.sh

--- toy_example/run_toy_example.sh
+++ toy_example/run_toy_example.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-p_in_expr_target=NetProphet_2.0/toy_example/zev_expr_500_100_indexed
-p_in_expr_reg=NetProphet_2.0/toy_example/zev_expr_reg_50_100_indexed
+p_wd=/home/vagrant/
+p_in_expr_target=${p_wd}NetProphet_2.0/toy_example/zev_expr_500_100_indexed
+p_in_expr_reg=${p_wd}NetProphet_2.0/toy_example/zev_expr_reg_50_100_indexed
 
 # de
-p_in_net_de=NetProphet_2.0/toy_example/zev_de_shrunken_50_500_indexed
+p_in_net_de=${p_wd}NetProphet_2.0/toy_example/zev_de_shrunken_50_500_indexed
 # lasso
 flag_global_shrinkage="ON"
 flag_local_shrinkage="OFF"
 
 # smoothing
-p_in_dbd_pids=NetProphet_2.0/toy_example/DBD_PIDS
+p_in_dbd_pids=${p_wd}NetProphet_2.0/toy_example/DBD_PIDS
 
 # pwn
-p_in_promoter=NetProphet_2.0/toy_example/promoter.scer.fasta
+p_in_promoter=${p_wd}NetProphet_2.0/toy_example/promoter.scer.fasta
 
 # logistics
 p_src_code=/home/vagrant/NetProphet_2.0/
 
 # singularity
 flag_singularity="ON"
-p_singularity_img=/home/vagrant/s_np3
+p_singularity_img=/home/vagrant/s_np.sif
 p_singularity_bindpath=/home/vagrant/
 
 # output
